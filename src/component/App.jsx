ea55da2e5473ec15da11e38589a052341bc1532b
anishdass <anishdassatoffice@gmail.com>
2024-08-28 21:01:43 +0100

    Rendered a table with a list of movies and deleting

---
+++ src/component/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Component } from "react";
+import Movies from "./movies";
+
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Movies/>
+      </main>
+    );
+  }
+}
+export default App;
