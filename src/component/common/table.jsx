fb089653f69cc72d5cdd1de251aa214b187cb01e
anishdass <anishdassatoffice@gmail.com>
2024-08-31 18:01:03 +0100

    Made a more precise movies table

---
+++ src/component/common/table.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TableHeader from "./tableheader";
+import TableBody from "./tablebody";
+
+const Table = (props) => {
+  const { columns, sortColumn, onSort, data } = props;
+  console.log(columns);
+  return (
+    <table className='table'>
+      <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+      <TableBody data={data} columns={columns} />
+    </table>
+  );
+};
+
+export default Table;
--- src/component/moviestable.jsx
+++ src/component/moviestable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import TableHeader from "./common/tableheader";
-import TableBody from "./common/tablebody";
 import Like from "./common/like";
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -27,16 +26,14 @@ class MoviesTable extends Component {
     },
   ];
   render() {
-    const { movies, onLike, onDelete, onSort, sortColumn } = this.props;
+    const { movies, onSort, sortColumn } = this.props;
     return (
-      <table className='table'>
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
